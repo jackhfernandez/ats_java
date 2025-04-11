e64e406e79049f2f6b8265b5d6d088ff77a2fd42
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-10 23:42:12 -0500

    Agregando Ejercicio

--- s01/src/main/java/conditionals/Ej03.java
+++ s01/src/main/java/conditionals/Ej03.java
@@ -1,7 +1,10 @@
 /**
- * 
+ * Ejercicio 14: Hacer un programa que pase de Kg a otra unidad de medida de
+ * masa, mostrar en pantalla un menú con las opciones posibles.
  */
 package conditionals;
+
+import java.util.Scanner;
 
 /**
  *
@@ -9,6 +12,35 @@ package conditionals;
  */
 public class Ej03 {
   public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+    double kg;
+    
+    System.out.println("Ingrese la cantidad en kg: ");
+    kg = scanner.nextDouble();
+    
+    System.out.println("Seleccione la unidad a la que desea convertir: ");
+    System.out.println("1. Gramos (g)");
+    System.out.println("2. Libras (lb)");
+    System.out.println("3. Toneladas (t)");
+    System.out.println("Opcion: ");
+    int opcion = scanner .nextInt();
+    
+    switch (opcion) {
+      case 1:
+        System.out.println(kg + " kg son " + kg * 1000 + " gramos");
+        break;
+      case 2:
+        System.out.println(kg + " kg son " + (kg * 2.20462) + " toneladas.");
+        break;
+      case 3:
+        System.out.println(kg + " kg son " + (kg /1000) + " toneladas.");
+        break;
+      default:
+        System.out.println("Opcion invalida");
+    }
+    
+    scanner.close();
     
   }
   
