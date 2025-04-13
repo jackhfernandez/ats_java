5a1f142becf1b48e6b7f0c2c4eee4377fd8b2763
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-12 23:22:44 -0500

    Agregando Do while

--- s01/src/main/java/While.java
+++ s01/src/main/java/While.java
@@ -1,6 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * 
  */
 
 /**
@@ -8,5 +7,12 @@
  * @author jackh
  */
 public class While {
-  
+  public static void main(String[] args) {
+    int contador = 1;
+
+        while (contador <= 5) {
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+  }
 }
