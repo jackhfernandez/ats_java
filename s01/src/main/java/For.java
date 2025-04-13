a311f3a8bd8d6a8f8c2aa70ad5915f9693084b3d
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-12 23:31:23 -0500

    Agregando Do while

---
+++ s01/src/main/java/For.java
@@ -0,0 +1,14 @@
+
+/**
+ *
+ * @author jackh
+ */
+public class For {
+  
+  public static void main(String[] args) {
+    
+    for (int i=1; i<=10; i++) {
+      System.out.println(i);
+    }
+  }
+}
