c1d7077d88a7166e5efe5bebaa99afca69958f08
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-15 22:56:28 -0500

    agregando ej05

---
+++ s01/src/main/java/Loops/Ej05.java
@@ -0,0 +1,34 @@
+/**
+ * Pedir numeros hasta que se ingrese cero. Calcular la media
+ */
+package Loops;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jackh
+ */
+public class Ej05 {
+  
+  public static void main(String[] args) {
+    
+    int numero, contador=0, suma=0; float media=0;
+    
+    numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero: "));
+    
+    do {
+      contador++;
+      suma +=numero;
+      numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese otro numero: "));
+    } while (numero !=0);
+    
+    if (contador == 0) {
+      System.out.println("Error. Division por cero");
+    } else {
+      media = (float) suma/contador; 
+      System.out.println("La media es " + media);
+    }    
+  }
+  
+}
