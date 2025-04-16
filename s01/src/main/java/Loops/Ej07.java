6db1064b0cf1710ddad871565bd0d1a832610ace
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-15 23:17:22 -0500

    agregando ej07

---
+++ s01/src/main/java/Loops/Ej07.java
@@ -0,0 +1,19 @@
+/**
+ * Mostrar los numeros desde el 100 hasta el 0 de 7 en 7
+ */
+package Loops;
+
+/**
+ *
+ * @author jackh
+ */
+public class Ej07 {
+  
+  public static void main(String[] args) {
+    
+    for (int i=100; i>=0; i-=7) {
+      System.out.print(i + " ");
+    }
+    
+  }
+}
