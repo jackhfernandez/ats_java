bd3e74c9c19c6b2a169c1da32c2536b11ff30f51
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-17 19:29:57 -0500

    avanzando ej13

---
+++ s01/src/main/java/Loops/Ej13.java
@@ -0,0 +1,19 @@
+/**
+ * Dada las edades y altura de 5 alumnos. Mostrar la edad y estatura media
+ * Mostrar alumnos mayores de 18
+ * Mostrar alumnos que miden mas de 1.75
+ */
+package Loops;
+
+/**
+ *
+ * @author jackh
+ */
+public class Ej13 {
+  public static void main(String[] args) {
+    int edad, estatura;
+    
+    edad = Integer.parseInt(JOptionPane.showInputDialog("Ingresa edad "))
+  }
+  
+}
