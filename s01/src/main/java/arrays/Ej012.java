7ce2fa25109ddee869517c0c35aee9bfacd59939
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-24 23:57:10 -0500

    agregando avance Ej012

---
+++ s01/src/main/java/arrays/Ej012.java
@@ -0,0 +1,23 @@
+/**
+ *  Leer por teclado una tabla de 10 elementos numéricos enteros y una
+ * posición (entre 0 y 9). Eliminar el elemento situado en la posición
+ * dada sindejar huecos.
+ */
+package arrays;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jackh
+ */
+public class Ej012 {
+  
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    int[] arreglo = new int[10];
+    
+    
+  }
+}
