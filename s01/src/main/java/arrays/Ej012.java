9cf4bebd32f75cda0ff564efd2034bf23e972e0f
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-25 23:45:43 -0500

    agregando Ej012

--- s01/src/main/java/arrays/Ej012.java
+++ s01/src/main/java/arrays/Ej012.java
@@ -17,7 +17,29 @@ public class Ej012 {
     Scanner scanner = new Scanner(System.in);
     
     int[] arreglo = new int[10];
+    int posicion;
     
+    // Llenar el arreglo con enteros
+    System.out.println("Ingrese 10 elementos: ");
+    for (int i=0; i<10; i++) {
+      arreglo[i] = scanner.nextInt();
+    }
     
+    do {
+      System.out.println("Ingrese la posicion a eliminar (0 - 10): ");
+      posicion = scanner.nextInt();
+    } while (posicion < 0 || posicion > 9);
+    
+    // Eliminar elemento en la posicion sin dejar hueco
+    for (int i=posicion; i<arreglo.length - 1; i++) {
+      arreglo[i] = arreglo[i+1];
+    }
+    
+    // Reducir el tamano logico del array
+    // Mostrar arreglo modificado
+    System.out.println("Arreglo modificado:");
+    for (int i=0; i<arreglo.length-1; i++) {
+      System.out.print(arreglo[i] + " ");
+    }
   }
 }
