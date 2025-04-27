b144e330ca7e3e8ad955a5d9b829a34783ec8fe8
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-26 21:44:46 -0500

    Terminando Ej014

--- s01/src/main/java/arrays/Ej014.java
+++ s01/src/main/java/arrays/Ej014.java
@@ -11,38 +11,55 @@ import java.util.Scanner;
  * @author jackh
  */
 public class Ej014 {
-  
+
   public static void main(String[] args) {
-    
+
     Scanner scanner = new Scanner(System.in);
-    
+
+    int cPares = 0, cImpares = 0;
     int[] arreglo = new int[10];
-    int[] pares = new int[5];
-    int[] impares = new int[5];
     
     // Recibiendo elementos
     System.out.println("Ingrese elementos: ");
-    for (int i=0; i<10; i++) {
-      arreglo[i] = scanner.nextInt();
-    }
-    
-    // almacenar en dos arrglos
-    for (int i=0; i<10; i++) {
-      if (arreglo[i] % 2 == 0 ) {
-        pares[i] = arreglo[i]; 
+    for (int i = 0; i < 10; i++) {
+      //arreglo[i] = scanner.nextInt();
+      arreglo[i] = i+1;
+
+      // Contar la cantidad de numeros pares e impares
+      if (arreglo[i] % 2 == 0) {
+        cPares++;
       } else {
-        impares[i] = arreglo[i];
+        cImpares++;
       }
     }
-    
-    System.out.println("Mostrando arreglos: ");
-    
-    for (int par : pares ) {
-      System.out.println(par + " ");
+
+    // Declarar e inicializar 
+    int[] impares = new int[cImpares];
+    int[] pares = new int[cPares];
+
+    // Inicializar el contador en cero para llenar los arreglos
+    cImpares = 0;
+    cPares = 0;
+
+    // almacenar en dos arreglos
+    for (int i = 0; i < 10; i++) {
+      if (arreglo[i] % 2 == 0) {
+        pares[cPares] = arreglo[i];
+        cPares++;
+      } else {
+        impares[cImpares] = arreglo[i];
+        cImpares++;
+      }
     }
-    
-    for (int impar : impares ) {
-      System.out.println(impar + " ");
+
+    System.out.println("Arreglo con numeros pares");
+    for (int par : pares) {
+      System.out.print(par + " ");
+    }
+
+    System.out.println("\n\nArreglo con numeros impares");
+    for (int impar : impares) {
+      System.out.print(impar + " ");
     }
   }
 }
