682e2cc51ee5562d01f59b8304ef375c3c4a46b7
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-26 23:59:21 -0500

    Agregando avance Ej017

---
+++ s01/src/main/java/arrays/Ej017.java
@@ -0,0 +1,20 @@
+/**
+ * Queremos desarrollar una aplicación que nos ayude a gestionar las notas de
+ * un centro educativo. Cada grupo (o clase) está compuesto por 5 alumnos.
+ * Se pide leer las notas del primer, segundo y tercer trimestre de un grupo.
+ * Debemos mostrar al final: la nota media del grupo en cada trimestre,
+ * y la medi del alumno que se encuentra en la posición N
+ * (N se pide por teclado)
+ */
+package arrays;
+
+/**
+ *
+ * @author jackh
+ */
+public class Ej017 {
+  
+  public static void main(String[] args) {
+    
+  }
+}
