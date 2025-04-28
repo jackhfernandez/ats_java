2497a7ddc37b5a5a4d986f7bc07a39167972f5fd
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-27 23:54:50 -0500

    Creando Insertion

---
+++ s01/src/main/java/sorting/Insertion.java
@@ -0,0 +1,15 @@
+/**
+ * Ordenamiento por el metodo de insercion
+ */
+package sorting;
+
+/**
+ *
+ * @author jackh
+ */
+public class Insertion {
+  
+  public static void main(String[] args) {
+    
+  }
+}
