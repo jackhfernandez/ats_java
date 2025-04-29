2011e1320d4ae28ac2dacb22dbced258c62cd2da
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-28 23:48:16 -0500

    Terminando Insertion

--- s01/src/main/java/sorting/Insertion.java
+++ s01/src/main/java/sorting/Insertion.java
@@ -3,6 +3,8 @@
  */
 package sorting;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jackh
@@ -11,5 +13,40 @@ public class Insertion {
   
   public static void main(String[] args) {
     
+    int[] arreglo = {};
+    int nElementos, aux, posicion;
+    
+    nElementos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de elementos: "));
+    
+    arreglo = new int[nElementos];
+    
+    // Agregando elementos al arreglo
+    for (int i=0; i<nElementos; i++) {
+      arreglo[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese elemento " + (i+1) + ": "));
+    }
+    
+    // Mostrando arreglo
+    System.out.println("Mostrando arreglo: ");
+    for (int arr : arreglo) {
+      System.out.print(arr + " ");
+    }
+    
+    // Ordenamiento por insercion
+    for (int i=0; i<nElementos; i++) {
+      posicion = i;
+      aux = arreglo[i];
+      
+      while ((posicion>0) && (arreglo[posicion-1] > aux )) {
+        arreglo[posicion] = arreglo[posicion-1];
+        posicion--;
+      }
+      arreglo[posicion] = aux;
+    }
+    
+    // Mostrando arreglo ordenado
+    System.out.print("\n\nMostrando arreglo ordenadao - ascendente\n");
+    for (int arr : arreglo ) {
+      System.out.print(arr + " ");
+    }
   }
 }
