2e5587f1775f04b969205d75806983e63cd6e73f
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-28 23:50:31 -0500

    Agregando busqueda Secuencial

---
+++ s01/src/main/java/sorting/BusquedaSecuencial.java
@@ -0,0 +1,15 @@
+/**
+ * Busqueda secuencial
+ */
+package sorting;
+
+/**
+ *
+ * @author jackh
+ */
+public class BusquedaSecuencial {
+  
+  public static void main(String[] args) {
+    
+  }
+}
