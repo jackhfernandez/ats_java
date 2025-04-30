5b7374438318a4ddf700502c7d2cc152b293457d
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-30 00:44:49 -0500

    Agregando EjemploMatriz

--- s01/src/main/java/matrices/EjemploMatriz.java
+++ s01/src/main/java/matrices/EjemploMatriz.java
@@ -3,6 +3,9 @@
  */
 package matrices;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jackh
@@ -11,5 +14,31 @@ public class EjemploMatriz {
   
   public static void main(String[] args) {
     
+    Scanner scanner = new Scanner(System.in);
+   
+    int matriz[][], nCol, nFilas;
+    
+    nFilas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de filas: "));
+    nCol = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de columnas: "));
+    
+    matriz = new int[nFilas][nCol];
+    
+    // Ingresar elementos
+    System.out.println("Ingrese elementos al arreglo");
+    for (int i=0; i<nFilas; i++) {
+      for (int j=0; j<nCol; j++) {
+        System.out.print("Matriz["+i+"]["+j+"] :");
+        matriz[i][j] = scanner.nextInt();
+      }
+    }
+    
+    // Mostrando arreglo
+    System.out.println("\nMostrando matriz");
+    for (int i=0; i<nFilas; i++) {
+      for (int j=0; j<nCol; j++) {
+        System.out.print(matriz[i][j] + " ");
+      }
+      System.out.println("");
+    }
   }
 }
