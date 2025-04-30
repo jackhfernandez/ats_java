7fe8788623683d12739d0b2c07aef38df2372078
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-04-29 23:36:48 -0500

    Terminando Busqueda secuencial

--- s01/src/main/java/sorting/BusquedaSecuencial.java
+++ s01/src/main/java/sorting/BusquedaSecuencial.java
@@ -3,6 +3,8 @@
  */
 package sorting;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jackh
@@ -11,5 +13,25 @@ public class BusquedaSecuencial {
   
   public static void main(String[] args) {
     
+    int[] arreglo = {4, 1, 5, 2, 3};
+    int dato;
+    boolean band = false;
+    
+    dato = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero a buscar: "));
+    
+    // Busqueda secuencial
+    int i=0;
+    while (i<5 && band == false) {
+      if (arreglo[i] == dato) {
+        band = true;
+      }
+      i++;
+    }
+    
+    if (band == false) {
+      JOptionPane.showMessageDialog(null, "No se ha encontrado el numero en el arreglo");
+    } else {
+      JOptionPane.showMessageDialog(null, "Nro encontrado en la posicion "+(i-1));
+    }
   }
 }
