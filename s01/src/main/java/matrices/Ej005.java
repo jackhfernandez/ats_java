7e616be8688161d9ab77d67629c1481261021b90
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-02 00:46:19 -0500

    Terminando Ej05

--- s01/src/main/java/matrices/Ej005.java
+++ s01/src/main/java/matrices/Ej005.java
@@ -4,7 +4,6 @@
  */
 package matrices;
 
-import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -13,29 +12,27 @@ import javax.swing.JOptionPane;
  */
 public class Ej005 {
 
-  // A nivel de clase
-  // Scanner scanner = new Scanner(System.in);
-
   public static void main(String[] args) {
 
-    int matriz[][], nFilas, nCol;
+    int matriz[][], nFilas, nCol, sFilas=0, sCol=0;
     nFilas = leerNumero("Ingrese el numero de filas");
     nCol = leerNumero("Ingrese el numero de columnas");
-    
+
     matriz = new int[nFilas][nCol];
-    
+
     agregarElementos(matriz, nFilas, nCol);
     mostrarMatriz(matriz, nFilas, nCol);
-    
+    sumaFilas(matriz, nFilas, nCol, sFilas);
+    sumaColumnas(matriz, nFilas, nCol, sCol);
+
   }
 
   // Agregar elementos a la matriz
   public static void agregarElementos(int matriz[][], int nFilas, int nCol) {
-        
-    matriz = new int[nFilas][nCol];
+
     for (int i = 0; i < nFilas; i++) {
       for (int j = 0; j < nCol; j++) {
-        matriz[i][j] = i + j;
+        matriz[i][j] = (int) (Math.random() * 9) + 1;
       }
     }
   }
@@ -46,14 +43,38 @@ public class Ej005 {
     numero = Integer.parseInt(JOptionPane.showInputDialog(mensaje));
     return numero;
   }
-  
-  public static void mostrarMatriz(int matriz[][], int nFilas, int nCol){
+
+  public static void mostrarMatriz(int matriz[][], int nFilas, int nCol) {
     System.out.println("Mostrando matriz");
-    for (int i=0; i<nFilas; i++) {
-      for (int j=0; j<nCol; j++) {
+    for (int i = 0; i < nFilas; i++) {
+      for (int j = 0; j < nCol; j++) {
         System.out.print(matriz[i][j] + "  ");
       }
       System.out.println("");
     }
+  }
+
+  // Sumar filas  
+  public static void sumaFilas(int matriz[][], int nFilas,int nCol, int sFilas) {
+    for (int i = 0; i < nFilas; i++) {
+      sFilas=0;
+      for (int j=0; j<nCol; j++){
+        sFilas += matriz[i][j];
+      }
+      System.out.print("\nLa suma de la fila ["+i+"] es "+ sFilas);
+    }
+    System.out.println("");
+  }
+  
+  // Sumar columnas
+  public static void sumaColumnas(int matriz[][], int nCol, int nFilas, int sCol){
+    for (int j=0; j<nCol; j++){
+      sCol = 0;
+      for (int i=0; i<nFilas; i++){
+        sCol += matriz[i][j];
+      }
+      System.out.print("\nLa suma de la columna ["+j+"] es: "+ sCol);
+    }
+    System.out.println("");
   }
 }
