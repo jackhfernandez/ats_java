8a4631d25eb805e614c048a6b59a21a3829eca44
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-01 23:57:41 -0500

    Agregando avance Ej006

---
+++ s01/src/main/java/matrices/Ej006.java
@@ -0,0 +1,16 @@
+/**
+ * Utilizando dos matrices de 5x9 y 9x5. cargar la primera y transponerla
+ * en la segunda
+ */
+package matrices;
+
+/**
+ *
+ * @author jackh
+ */
+public class Ej006 {
+  
+  public static void main(String[] args) {
+    
+  }
+}
