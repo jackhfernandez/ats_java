8332b60646aceb5d7ce7ed679d3872fd2095617e
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-02 23:53:06 -0500

    Agregando avance Ej007

---
+++ s01/src/main/java/matrices/Ej007.java
@@ -0,0 +1,16 @@
+/**
+ *  Crear una matriz “marco” de tamaño 5x5: todos sus elementos deben ser O
+ *  salvo los de los bordes que deben ser 1. Mostrarla.
+ */
+package matrices;
+
+/**
+ *
+ * @author jackh
+ */
+public class Ej007 {
+  
+  public static void main(String[] args) {
+    
+  }
+}
