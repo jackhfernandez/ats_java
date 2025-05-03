b8e630f27a58509e2d74198b3cc2a64015602376
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-03 00:18:43 -0500

    Terminando Ej008

--- s01/src/main/java/matrices/Ej008.java
+++ s01/src/main/java/matrices/Ej008.java
@@ -12,5 +12,36 @@ public class Ej008 {
   
   public static void main(String[] args) {
     
+    int matriz[][] = new int[5][5];
+
+    agregarElementos(matriz);
+    mostrarMatriz(matriz);
+    
+  }
+  
+  // Agregar elementos 
+  public static void agregarElementos(int matriz[][]){
+    for (int i=0; i<5; i++){
+      for (int j=0; j<5; j++){
+        if (i == 0 || i == 4){
+          matriz[i][j] = 1;
+        } else if (j == 0 || j == 4) {
+          matriz[i][j] = 1;
+        } else {
+          matriz[i][j] = 0;
+        }
+      }
+    }
+  }
+  
+  // Mostrando
+  public static void mostrarMatriz(int matriz[][]){
+    System.out.println("Mostrando matriz");
+    for (int i=0; i<5; i++){
+      for (int j=0; j<5; j++){
+        System.out.print(matriz[i][j] + "  ");
+      }
+      System.out.println("");
+    }
   }
 }
