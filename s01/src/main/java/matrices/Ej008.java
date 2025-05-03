ad4c29eaf063f622352cb7f69776b40d353fdb5e
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-02 23:56:29 -0500

    Agregando avance Ej008

---
+++ s01/src/main/java/matrices/Ej008.java
@@ -0,0 +1,16 @@
+/**
+ * Crear y cargar una matriz de 10 elementos. Pedir al usuario dos posiciones
+ * de la matriz e intercambiar de posicion
+ */
+package matrices;
+
+/**
+ *
+ * @author jackh
+ */
+public class Ej008 {
+  
+  public static void main(String[] args) {
+    
+  }
+}
