5ab0e29309ac98697f2c9d6240c84cce385c39ff
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-04 23:02:11 -0500

    Agregando package contructores

---
+++ POO/src/main/java/Contructores/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Funcion principal. Constructores
+ */
+
+package Contructores;
+
+/**
+ *
+ * @author jackh
+ */
+public class Main {
+  
+  public static void main(String[] args) {
+    
+    Persona p1 = new Persona("Jack", 20);
+    
+    p1.mostrarDatos();
+  }
+}
---
+++ POO/src/main/java/Contructores/Persona.java
@@ -0,0 +1,27 @@
+/**
+ * Metodo constructor
+ */
+package Contructores;
+
+/**
+ *
+ * @author jackh
+ */
+public class Persona {
+  // Atributos
+  String nombre;
+  int edad;
+  
+  // Metodos
+  
+  // Metodo constructor
+  public Persona(String nombre, int edad){
+    this.nombre = nombre;
+    this.edad   = edad;
+  }
+  
+  public void mostrarDatos(){
+    System.out.println("El nombre es: "+ nombre);
+    System.out.println("La  edad  es: "+ edad);
+  }
+}
