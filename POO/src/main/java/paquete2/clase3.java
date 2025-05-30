c8cc5bba4074cabe273f453befc3031f4d1b9e03
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-29 23:58:32 -0500

    New commit

--- POO/src/main/java/paquete2/clase3.java
+++ POO/src/main/java/paquete2/clase3.java
@@ -1,6 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * volviendo a esto despues de tiempos
  */
 package paquete2;
 
